9f4e58d9ceb76f4890e5bf63421e29b2da9b43ae
Derek Chen <derekzc99@gmail.com>
2023-12-06 15:06:25 -0800

    useEffect with counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,33 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 // a component is a function that returns JSX
-const Person = (props) => {
-  return (
-    <>
-      <h1>Name: {props.name}</h1>
-      <h2>Last Name: {props.lastName}</h2>
-      <h2>Age: {props.age}</h2>
-    </>
-  )
-}
 
-// class is a reserved keyword in JS, so we use className
 const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    alert("You've changed the counter to " + counter)
+  }, [counter])
+
+  // This function return the prevCount minus 1
+  function decrement_cnt(prevCount) {
+    return prevCount - 1;
+  }
+
+  function clickHandler() {
+    setCounter(decrement_cnt); // We must use func for setter because the 
+    // the value to set depends on prev value
+  } // (1) Simple case, we know the value: setCounter(3)
+    // (2) The value to set depends on prev value, so here it is the
+    //     is the return value of func
+
   return (
     <div className="App">
-      <Person name="John" lastName={'Doe'} age={25}/>
-      <Person name="Mary" lastName={'Doe'} age={24} />
+      {/* class is a reserved keyword in JS, so we use className */}
+      <button onClick={clickHandler}>-</button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1 )}>+</button>
     </div>
   );
 }
