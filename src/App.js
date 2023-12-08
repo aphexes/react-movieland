41d0e32a218213435238591f367e1c59500cb1a9
Derek Chen <derekzc99@gmail.com>
2023-12-07 17:09:32 -0800

    minor fixes

--- src/App.js
+++ src/App.js
@@ -7,14 +7,6 @@ import SearchIcon from './search.svg'
 
 const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f0db519c'
 
-const movie1 = {
-  "Title": "Andrei Rublev",
-  "Year": "1966",
-  "imdbID": "tt0060107",
-  "Type": "movie",
-  "Poster": "https://m.media-amazon.com/images/M/MV5BM2U2MzYwMzAtM2Q2Zi00OTJhLTgxMjEtOGM0NDk5Mzk4MzE5L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjMxMjkwMjI@._V1_SX300.jpg"
-}
-
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
@@ -28,7 +20,7 @@ const App = () => {
   }
 
   useEffect(() => {
-    searchMovies('Andrei Rublev')
+    searchMovies('Batman')
   }, [])
 
   return (
