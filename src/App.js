3735e9865a02fa2e2dfa0abefcd4a3346575da7b
Derek Chen <derekzc99@gmail.com>
2023-12-07 17:18:56 -0800

    secure https omdb

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import MovieCard from "./MovieCard";
 import './App.css'
 import SearchIcon from './search.svg'
 
-const API_URL = 'http://www.omdbapi.com/?i=tt3896198&apikey=f0db519c'
+const API_URL = 'https://www.omdbapi.com/?i=tt3896198&apikey=f0db519c'
 
 const App = () => {
   const [movies, setMovies] = useState([]);
